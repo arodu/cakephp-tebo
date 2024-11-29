9b373de379568865fde7c83c24b1ed4c1d508eed
Alberto Rodriguez <arodu.dev@gmail.com>
2024-11-29 17:28:14 -0400

    feat: add chat action methods to Chat class for improved Telegram interaction

--- src/Telegram/Chat.php
+++ src/Telegram/Chat.php
@@ -6,6 +6,7 @@ namespace TeBo\Telegram;
 
 use Cake\Log\Log;
 use InvalidArgumentException;
+use TeBo\Enum\TelegramMethod;
 use TeBo\Response\ResponseInterface;
 use TeBo\Utility\Bot;
 use TeBo\Utility\Trait\DataManageTrait;
@@ -14,6 +15,17 @@ use TeBo\Telegram\Api as TelegramApi;
 class Chat
 {
     use DataManageTrait;
+
+    const CHAT_ACTION_TYPING = 'typing';
+    const CHAT_ACTION_UPLOAD_PHOTO = 'upload_photo';
+    const CHAT_ACTION_RECORD_VIDEO = 'record_video';
+    const CHAT_ACTION_UPLOAD_VIDEO = 'upload_video';
+    const CHAT_ACTION_RECORD_AUDIO = 'record_audio';
+    const CHAT_ACTION_UPLOAD_AUDIO = 'upload_audio';
+    const CHAT_ACTION_UPLOAD_DOCUMENT = 'upload_document';
+    const CHAT_ACTION_FIND_LOCATION = 'find_location';
+    const CHAT_ACTION_RECORD_VIDEO_NOTE = 'record_video_note';
+    const CHAT_ACTION_UPLOAD_VIDEO_NOTE = 'upload_video_note';
 
     protected int $id;
     protected ?array $lastResult = null;
@@ -70,5 +82,32 @@ class Chat
         Bot::debug('Chat response: ', $this->lastResult);
 
         return $this->lastResult['ok'] ?? false;
+    }
+
+    /**
+     * Send chat action.
+     *
+     * @param string $action The action to send.
+     * @return boolean
+     */
+    public function chatAction(string $action): bool
+    {
+        $this->lastResult = TelegramApi::call(TelegramMethod::SEND_CHAT_ACTION, [
+            'chat_id' => $this->id,
+            'action' => $action,
+        ]);
+        Bot::debug('Chat action response: ', $this->lastResult);
+
+        return $this->lastResult['ok'] ?? false;
+    }
+
+    /**
+     * Send chat action typing.
+     *
+     * @return boolean
+     */
+    public function chatActionTyping(): bool
+    {
+        return $this->chatAction(self::CHAT_ACTION_TYPING);
     }
 }
